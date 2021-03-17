030b0c4dd02a186b8f1e37847bf815536a482f63
Robin Kolk <robinkolk@msn.com>
2021-03-17 21:55:57 +0100

    uart read

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, machine, network, gc, threading, app.secrets as secrets
+import time, machine, network, gc, ubinascii, app.secrets as secrets
 from machine import UART
 
 def connectToWifiAndUpdate():
@@ -23,18 +23,54 @@ def connectToWifiAndUpdate():
         del(otaUpdater)
         gc.collect()
 
+
+class RunUartConnection():
+    def __init__(self, baudrate):
+        self.run = 0
+        self.baudrate = baudrate
+
+        # init UART
+        self.init_uart()
+
+    def test_uart(self):
+        while True:
+            #jacuzzi_buffer = bytearray("", 'utf-8')
+            #remote_buffer = bytearray("", 'utf-8')
+            #jacuzzi_buffer = self.uart1_jacuzzi.read()
+            remote_buffer = self.uart2_remote.read()
+
+            #if jacuzzi_buffer is not "":
+            #    print("uart1")
+            #    print(jacuzzi_buffer)
+            #    if jacuzzi_buffer is not None:
+            #        print(ubinascii.hexlify(jacuzzi_buffer))
+            #        print(ubinascii.b2a_base64(jacuzzi_buffer))
+
+            if remote_buffer is not "":
+               # print("uart2")
+                if remote_buffer is not None:
+                    print(remote_buffer)
+                    print(ubinascii.hexlify(remote_buffer))
+                    print(ubinascii.b2a_base64(remote_buffer))
+
+
+    def init_uart(self):
+        # uart1 = Jacuzzi
+        self.uart1_jacuzzi = UART(1, baudrate=self.baudrate, tx=14, rx=4, bits=7, stop=1, parity=None)
+
+        # uart2 = remote
+        self.uart2_remote = UART(2, baudrate=self.baudrate, tx=15, rx=27, bits=7, stop=1, parity=None)
+        print(" baudrate inits at = " + str(self.baudrate))
+
+    def de_init_uart(self):
+        self.uart1_jacuzzi.deinit()
+        self.uart2_remote.deinit()
+
 def startApp():
-    event = threading.Event()
-    # uart1 = Jacuzzi
-    uart1 = UART(1, baudrate=2400, tx=4, rx=14)
-    # uart2 = remote
-    uart2 = UART(2, baudrate=2400, tx=15, rx=27)
-    while True:
-        print("uart1")
-        print(uart1.read())
-        print("uart2")
-        print(uart2.read())
-        event.wait(0.75)
-        
+    baud_9600 = RunUartConnection(baudrate=4800)
+    baud_9600.test_uart()
+    baud_9600.de_init_uart()
+
+
 connectToWifiAndUpdate()
 startApp()
