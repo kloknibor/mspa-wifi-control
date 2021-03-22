3da1d79df3884df4928710562badc0566fbe3237
Robin Kolk <robinkolk@msn.com>
2021-03-22 16:17:36 +0100

    Clean up and more commands from jacuzzi

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import time, machine, network, gc, ubinascii, app.secrets as secrets
+import app.secrets as secrets
+import gc
+import machine
+import network
+import time
+
 from machine import UART
 
 
@@ -37,6 +42,7 @@ class RunUartConnection():
         self.last_message_x07 = []
         self.last_message_x08 = []
         self.last_message_r = []
+        self.last_message_x0b = []
 
         # current states
         self.jacuzzi_temp = 0
@@ -45,30 +51,22 @@ class RunUartConnection():
         self.filter_state_on = False
         self.heating_temp = 0
 
-
         # init UART
-        self.init_uart()
-
+        self.uart1_jacuzzi = UART(1, baudrate=9600, tx=14, rx=4, bits=8, stop=1, parity=None)
+        self.uart2_remote = UART(2, baudrate=9600, tx=15, rx=27, bits=8, stop=1, parity=None)
 
     def test_uart(self):
         while True:
             if self.uart2_remote.any():
                 # self.uart2_remote.readinto(self.remote_buffer)
                 remote_buffer = self.uart2_remote.readline()
-                self.process_message(remote_buffer)
+                self.process_message(remote_buffer, "remote")
                 self.uart1_jacuzzi.write(bytearray(remote_buffer))
 
             if self.uart1_jacuzzi.any():
                 jacuzzi_buffer = self.uart1_jacuzzi.readline()
+                self.process_message(jacuzzi_buffer, "jacuzzi")
                 self.uart2_remote.write(bytearray(jacuzzi_buffer))
-
-    def init_uart(self):
-        # uart1 = Jacuzzi
-        self.uart1_jacuzzi = UART(1, baudrate=9600, tx=14, rx=4, bits=8, stop=1, parity=None)
-
-        # uart2 = remote
-        self.uart2_remote = UART(2, baudrate=9600, tx=15, rx=27, bits=8, stop=1, parity=None)
-        # print(" baudrate inits at = " + str(self.baudrate))
 
     def de_init_uart(self):
         self.uart1_jacuzzi.deinit()
@@ -118,7 +116,6 @@ class RunUartConnection():
         # sturen : self.uart1_jacuzzi.write(bytearray(b'\xa5\x02\x00\xa7'))
         pass
 
-
     def split_message_to_array(self, message):
         message_str = str(message)
         msg = message_str.split("\'")
@@ -146,7 +143,7 @@ class RunUartConnection():
         # print(messages)
         return messages
 
-    def process_message(self, message):
+    def process_message(self, message, sender):
         # split message to usefull array
         messages_array = self.split_message_to_array(message=message)
 
@@ -169,69 +166,88 @@ class RunUartConnection():
                     if message[1] == "x01":
                         if self.last_message_x01 != message:
                             self.last_message_x01 = message
-                            print("still unkown message : " + str(message))
+                            if message[2] == "x00":
+                                print(" Heater is uit " + str(message) + str(sender))
+                                self.filter_state_on = False
+                            elif message[2] == "x01":
+                                self.filter_state_on = True
+                                print(" Heater is aan" + str(message) + str(sender))
+                            else:
+                                print(" unkown Heater state " + str(message) + str(sender))
                     # filter aan x02
                     elif message[1] == "x02":
                         if self.last_message_x02 != message:
                             self.last_message_x02 = message
                             if message[2] == "x00":
-                                print(" Filter is uit " + str(message))
+                                print(" Filter is uit " + str(message) + str(sender))
                                 self.filter_state_on = False
                             elif message[2] == "x01":
                                 self.filter_state_on = True
-                                print(" Filter is aan" + str(message))
+                                print(" Filter is aan" + str(message) + str(sender))
                             else:
-                                print(" unkown filter state " + str(message))
+                                print(" unkown filter state " + str(message) + str(sender))
 
                     # bubbels aan x03
                     elif message[1] == "x03":
                         if self.last_message_x03 != message:
                             self.last_message_x03 = message
                             if message[2] == "x00":
-                                print(" Bubbels zijn uit ")
+                                print(" Bubbels zijn uit " + str(sender))
                                 self.bubbel_state_on = False
                             elif message[2] == "x01":
                                 self.bubbel_state_on = True
-                                print(" Bubbels zijn aan")
+                                print(" Bubbels zijn aan" + str(sender))
                             else:
-                                print(" unkown bubbel state " + str(message))
+                                print(" unkown bubbel state " + str(message) + str(sender))
                     elif message[1] == "x04":
                         if self.last_message_x04 != message:
                             self.last_message_x04 = message
                             temp_val = message[2].replace('x', '')
                             # maximum setting seems to be 31! Bug in Remote? Lie in specs?
                             print(
-                                "The temprature on remote is set to : " + str(int(temp_val, 16)) + "   " + str(message))
+                                "The temprature on remote is set to : " + str(int(temp_val, 16)) + "   " + str(
+                                    message) + str(sender))
                             self.jacuzzi_temp = int(temp_val, 16)
                     elif message[1] == "x05":
                         if self.last_message_x05 != message:
                             self.last_message_x05 = message
-                            print("still unkown message : " + str(message))
+                            print("still unkown message : " + str(message) + str(sender))
                     # temp van jacuzzi x06
                     elif message[1] == "x06":
                         if self.last_message_x06 != message:
                             self.last_message_x06 = message
                             temp_hex_jac = message[2].replace('x', '')
                             temp_jac = ((int(temp_hex_jac, 16)) * 10) / 2
-                            print("The jacuzzi temp is : " + str(temp_jac) + " devide by 10 ")
+                            print("The jacuzzi temp is : " + str(temp_jac) + " devide by 10 " + str(sender))
                     elif message[1] == "x07":
                         if self.last_message_x07 != message:
                             self.last_message_x07 = message
-                            print("still unkown message (bit = x07): " + str(message))
+                            print("still unkown message (bit = x07): " + str(message) + str(sender))
                     elif message[1] == "x08":
                         if self.last_message_x08 != message:
                             self.last_message_x08 = message
-                            print("still unkown message (bit = x08): " + str(message))
-                    if message[1] == "r":
+                            if message[2] == "x00":
+                                print(" Filter is uit vanaf jacuzzi" + str(message) + str(sender))
+                            elif message[2] == "x03":
+                                print(" Filter is zonder error aan vanaf jacuzzi" + str(message) + str(sender))
+                            else:
+                                print(" unkown Heater state " + str(message) + str(sender))
+                    elif message[1] == "r":
                         if self.last_message_r != message:
                             self.last_message_r = message
-                            print("still unkown message (bit = r): " + str(message))
+                            print("still unkown message (bit = r): " + str(message) + str(sender))
+
+                    elif message[1] == "x0b":
+                        if self.last_message_x0b != message:
+                            self.last_message_x0b = message
+                            print("still unkown message (bit = x0b): " + str(message) + str(sender))
 
 
                     else:
-                        print("still unkown message : " + str(message))
+                        print("still unkown message : " + str(message) + str(sender))
+                        print("bit 2 was:" + message[1])
                 else:
-                    print("verify failed, mesages wrong " + str(message))
+                    print("verify failed, mesages wrong " + str(message) + str(sender))
 
 
 def startApp():
@@ -239,6 +255,5 @@ def startApp():
     connect.test_uart()
 
 
-
-# connectToWifiAndUpdate()
+connectToWifiAndUpdate()
 startApp()
